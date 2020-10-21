bac7b35396eb44e791c8905504c8548fa69b1b6d
SuhejlHadzi <57288685+SuhejlHadzi@users.noreply.github.com>
2020-10-21 16:28:03 +0300

    Updates

---
+++ precache-manifest.7f79828525407177cbfc62227024e57a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d6861ab4a4c8d02b8a610d0e3cf1d7",
+    "url": "/movie-library/index.html"
+  },
+  {
+    "revision": "6d196d5134c660c3810b",
+    "url": "/movie-library/static/js/2.167bf3af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-library/static/js/2.167bf3af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960f93766230b7c05306",
+    "url": "/movie-library/static/js/main.f888d1a8.chunk.js"
+  },
+  {
+    "revision": "013c70500261c060a513",
+    "url": "/movie-library/static/js/runtime-main.cba2209a.js"
+  }
+]);
